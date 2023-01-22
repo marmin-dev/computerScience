9b8a6087848b6caad31424e264aca940f053f3e5
Marmin <marmin.dev@gmail.com>
2023-01-22 13:48:08 +0900

    study about data structure

---
+++ dataStructure/ch5-2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+string a;
+int n = 10;
+int main()
+{
+    for(int i =0; i <10; i++){
+        for(int j =0; j<n;j++){
+            for(int k = 0; k<n;k++){
+                if(true){
+                    cout << k << '\n';
+                }
+            }
+        }
+    }
+    for(int i =0;i<n;i++){
+        if(true) cout << i <<'\n';
+    }
+}
---
+++ dataStructure/ch5-4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[10];
+int main()
+{
+  for(int i =0; i<10;i++)a[i] = i;
+  for(auto it : a) cout << it << " ";
+  cout << '\n';
+  return 0;
+}
